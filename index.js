5951ed9ec565ae7058e6b5f16045ca1c562ced29
intercambioneuronal <intercambioneuronal@gmail.com>
2023-03-08 16:21:59 -0500

    rm comentado

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
 import fetch from 'node-fetch';
 import { navigate,toggleVisible } from './services/formatter.js';
 import { app } from './app.mjs';
-
-/*
-const urlString=document.getElementById("urlapi")?.textContent
-const btnMostrar=document.getElementById("mostrar")
-// https://pokeapi.co/api/v2/pokemon/ditto
-
-
-window.addEventListener("load", async function (event) {
-    const response = await fetch(urlString)
-    .then(res=>res.json());    
-    btnMostrar.addEventListener("click",()=>{
-        let parsed = parseNull(response)
-        if (parsed) {
-            document.getElementById("contentApi").innerHTML = navigate(parsed)
-        }
-        Array.from(document.getElementsByTagName("property")).forEach(item => {
-            item.addEventListener('click', event => {                
-                let value = event.currentTarget.nextElementSibling;
-                toggleVisible(value, "hide")
-            })
-        })        
-    })
-});
-*/
